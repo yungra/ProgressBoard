429a854b2d684f34a480c85edaa88f9e95296299
yungra <yhuga01@gmail.com>
2023-01-29 16:54:42 +0900

    todo機能にて、タイトルをProgressBoardに変更

--- resources/views/student/todo/show.blade.php
+++ resources/views/student/todo/show.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel Vite Vue.js 3</title>
+    <title>ProgressBoard</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
