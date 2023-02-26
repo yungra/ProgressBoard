1e2599e51d8f6d4e677f365f9dcf43404608ceff
koudai <koudaisports26@gmail.com>
2023-02-26 15:32:57 +0900

    pusherを受信確認

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,37 +18,23 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
-
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
-
-// Ably
-
 //import→あるモジュールでエクスポートされた、関数やオブジェクト、プリミティブ値を別モジュール内にインポートするために使う文法
 //import {モジュール名} from "インポート元"
 
+//pusherのgetting startedのコードを参考
 import Echo from "laravel-echo";
-// import Pusher from 'pusher-js';
+import Pusher from "pusher-js"; //追加
 
-//window→画面上に表示されているすべてのオブジェクトの親となるオブジェクトで、JavaScriptのオブジェクト階層の最上位に位置する
-// window.Pusher = Pusher;
+window.Echo = new Echo({
+    broadcaster: "pusher",
+    key: "0e96ccee8f00cfa2cfdf",
+    cluster: "ap3",
+    forceTLS: true,
+});
 
-// window.Echo = new Echo({
-//     broadcaster: "pusher",
-//     key: import.meta.env.VITE_ABLY_PUBLIC_KEY,
-//     wsHost: "realtime-pusher.ably.io",
-//     wsPort: 443,
-//     disableStats: true,
-//     encrypted: true,
-// });
+var channel = window.Echo.channel("task-added-channel"); //チャンネル名変更
+channel.listen("TaskAdded", function (data) {
+    //イベント名変更
+    console.log("received");
+    console.log(data);
+});
